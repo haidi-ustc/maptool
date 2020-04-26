567b2a541cbea95773dbcf956290a8a301b946ef
haidi <haidi@mail.ustc.edu.cn>
2020-04-26 23:00:25 +0800

    reset README

---
+++ doc/md2rst.py
@@ -0,0 +1,26 @@
+import requests
+
+def md_to_rst(from_file, to_file):
+    """
+    @param from_file: {str} markdown
+    @param to_file: {str} rst
+    """
+    response = requests.post(
+        url='http://c.docverter.com/convert',
+        data={'to': 'rst', 'from': 'markdown'},
+        files={'input_files[]': open(from_file, 'rb')}
+    )
+
+    if response.ok:
+        with open(to_file, "wb") as f:
+            f.write(response.content)
+    else:
+        print("You need convert .md file to .rst online:")
+        print("https://docverter.com/")
+
+if __name__ == '__main__':
+<<<<<<< Updated upstream
+    md_to_rst("../README.md", "maindoc.rst")
+=======
+    md_to_rst("../README.md", "main.rst")
+>>>>>>> Stashed changes
